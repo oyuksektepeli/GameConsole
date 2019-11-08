416b6bceec9b0d5177f38ff4d531f7c7e0af4ddd
Onur Yüksektepeli <onuryuksektepeli@hotmail.com.tr>
2019-11-08 14:30:35 +0300

    Add project files.

---
+++ GameConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GameConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
